a0242c826bc15d5eb1eef0f95f1ef6432a918609
theshrish46 <theshrish46@gmail.com>
2022-10-31 15:35:45 +0530

    Updated the code for small screen sizes

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const continueButton = document.GetElementById('subbtn')
-const cardHolderName = document.GetElementById('name')
-const cardNumber = document.GetElementById('number')
-const month = document.GetElementById('date-m')
-const year = document.GetElementById('date-y')
-const cvc = document.GetElementById('cvc')
+const cardName = document.getElementById('name')
+const cardNumber = document.getElementById('number')
+const month = document.getElementById('date-m')
+const year = document.getElementById('date-y')
+const cvcNumber = document.getElementById('cvc')
 
 
-const surveyPage = document.getElementsByClassName('form-sec')
-const thankyouPage = document.getElementsByClassName('thank')
+const frontPage = document.querySelector('.form-sec')
+const resultPage = document.querySelector('.thank')
+
+resultPage.style.display = 'none'
 
 
-continueButton.addEventListener('click', function () {
-    alert("hai")
-    
-    if(cardHolderName.innerHTML == " "){
-        alert("empty")
-    }
-    else{
-        alert("not empty")
+const submitButton = document.querySelector('#submit-button')
+submitButton.addEventListener('click', function(){
+    if(cardName.textContent != " " && cardNumber.textContent != " " && month.textContent != " " && year.textContent != " " && cvcNumber.textContent != " "){
+        frontPage.style.display = 'none'
+        resultPage.style.display = 'block'
+        
     }
 })
-
