ad34f131ee23deb19d03d322ff587fd1b056073b
theshrish46 <theshrish46@gmail.com>
2022-11-02 23:43:05 +0530

    did some minor changes to size

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ resultPage.style.display = 'none'
 
 const submitButton = document.querySelector('#submit-button')
 submitButton.addEventListener('click', function(){
-    if(cardName.textContent != " " && cardNumber.textContent != " " && month.textContent != " " && year.textContent != " " && cvcNumber.textContent != " "){
+    if(cardName.textContent != " " || cardNumber.textContent != " " || month.textContent != " " || year.textContent != " " || cvcNumber.textContent != " "){
         frontPage.style.display = 'none'
         resultPage.style.display = 'block'
         
